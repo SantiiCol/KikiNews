3f2dc5bafb5b412bd33c677176ad894cd1db71a3
SantiiCol <santiiagoverde10@gmail.com>
2021-05-06 22:00:36 -0500

    first commit

---
+++ js/sweetAlert.js
@@ -0,0 +1,60 @@
+Swal.fire({
+  title: "Actualizaciones de la pagina!",
+  //   text: "estas son las actualizaciones de la pagina",
+  html:
+    " <ul> <li> -NUEVAS NOTICIAS EN LA PAGINA!</li> <li> -WETSCOL PAGUE PUES EL HOST</li> <li>-FELICITACIONES A PESQUE POR EL PARTNER!! <3</li> <li> -SANTICOL X JUANFE</li>  </ul> ",
+  // icon:
+  // confirmButtonText:
+  footer: "<span> Informacion Importante </span>",
+  width: "50%",
+   padding: "4rem",
+  //background:
+  // grow:
+  backdrop: true,
+  // timer:
+  // timerProgressBar:
+  // toast:
+  // position:
+  allowOutsideClick: true,
+  // allowEscapeKey:
+  allowEnterKey: true,
+  // stopKeydownPropagation:
+
+  // input:
+  // inputPlaceholder:
+  // inputValue:
+  // inputOptions:
+
+  customClass: {
+    // //     container:
+    popup: "popup-class",
+    // //     header:
+    //  title:
+    // //     closeButton:
+    // //     icon:
+    // //     image:
+    // //     content:
+    // //     input:
+    // //     actions:
+    // //     confirmButton:
+    // //     cancelButton:
+    // //    footer:
+  },
+  // showConfirmButton:
+  // confirmButtonColor:
+  // confirmButtonAriaLabel:
+
+  // showCancelButton:
+  // cancelButtonText:
+  // cancelButtonColor:
+  // cancelButtonAriaLabel:
+
+  // buttonsStyling:
+  // showCloseButton:
+  // closeButtonAriaLabel:
+
+  // imageUrl:
+  // imageWidth:
+  // imageHeight:
+  // imageAlt:
+});
